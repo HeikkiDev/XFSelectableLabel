9198efe3fea1722f7f4cbd713299abfa7e57cc96
Enrique Ramos Vargas <enri-13@hotmail.com>
2018-11-24 01:35:53 +0100

    Xamarin Forms Selectable Label Example Project

---
+++ SelectableLabelApp/SelectableLabelApp.Android/Renderers/SelectableLabelRenderer.cs
@@ -0,0 +1,106 @@
+﻿using System.ComponentModel;
+using Android.Content;
+using Android.Widget;
+using SelectableLabelApp.CustomControls;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.Android;
+
+[assembly: ExportRenderer(typeof(SelectableLabel), typeof(SelectableLabelApp.Droid.Renderers.SelectableLabelRenderer))]
+namespace SelectableLabelApp.Droid.Renderers
+{
+    public class SelectableLabelRenderer : ViewRenderer<SelectableLabel, TextView>
+    {
+        TextView textView;
+
+        public SelectableLabelRenderer(Context context) : base(context)
+        {
+
+        }
+
+        protected override void OnElementChanged(ElementChangedEventArgs<SelectableLabel> e)
+        {
+            base.OnElementChanged(e);
+
+            var label = (SelectableLabel)Element;
+            if (label == null)
+                return;
+
+            if (Control == null)
+            {
+                textView = new TextView(this.Context);
+            }
+
+            textView.Enabled = true;
+            textView.Focusable = true;
+            textView.LongClickable = true;
+            textView.SetTextIsSelectable(true);
+
+            // Initial properties Set
+            textView.Text = label.Text;
+            textView.SetTextColor(label.TextColor.ToAndroid());
+            switch (label.FontAttributes)
+            {
+                case FontAttributes.None:
+                    textView.SetTypeface(null, Android.Graphics.TypefaceStyle.Normal);
+                    break;
+                case FontAttributes.Bold:
+                    textView.SetTypeface(null, Android.Graphics.TypefaceStyle.Bold);
+                    break;
+                case FontAttributes.Italic:
+                    textView.SetTypeface(null, Android.Graphics.TypefaceStyle.Italic);
+                    break;
+                default:
+                    textView.SetTypeface(null, Android.Graphics.TypefaceStyle.Normal);
+                    break;
+            }
+
+            textView.TextSize = (float)label.FontSize;
+
+            SetNativeControl(textView);
+        }
+
+        protected override void OnElementPropertyChanged(object sender, PropertyChangedEventArgs e)
+        {
+            base.OnElementPropertyChanged(sender, e);
+
+            if (e.PropertyName == SelectableLabel.TextProperty.PropertyName)
+            {
+                if (Control != null && Element != null && !string.IsNullOrWhiteSpace(Element.Text))
+                {
+                    textView.Text = Element.Text;
+                }
+            }
+            else if (e.PropertyName == SelectableLabel.TextColorProperty.PropertyName)
+            {
+                if (Control != null && Element != null)
+                {
+                    textView.SetTextColor(Element.TextColor.ToAndroid());
+                }
+            }
+            else if (e.PropertyName == SelectableLabel.FontAttributesProperty.PropertyName
+                        || e.PropertyName == SelectableLabel.FontSizeProperty.PropertyName)
+            {
+                if (Control != null && Element != null)
+                {
+                    switch (Element.FontAttributes)
+                    {
+                        case FontAttributes.None:
+                            textView.SetTypeface(null, Android.Graphics.TypefaceStyle.Normal);
+                            break;
+                        case FontAttributes.Bold:
+                            textView.SetTypeface(null, Android.Graphics.TypefaceStyle.Bold);
+                            break;
+                        case FontAttributes.Italic:
+                            textView.SetTypeface(null, Android.Graphics.TypefaceStyle.Italic);
+                            break;
+                        default:
+                            textView.SetTypeface(null, Android.Graphics.TypefaceStyle.Normal);
+                            break;
+                    }
+
+                    textView.TextSize = (float)Element.FontSize;
+                }
+            }
+        }
+    }
+}
---
+++ SelectableLabelApp/SelectableLabelApp.iOS/Renderers/SelectableLabelRenderer.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.ComponentModel;
+using UIKit;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.iOS;
+using SelectableLabelApp.CustomControls;
+
+[assembly: ExportRenderer(typeof(SelectableLabel), typeof(SelectableLabelApp.iOS.Renderers.SelectableLabelRenderer))]
+namespace SelectableLabelApp.iOS.Renderers
+{
+    public class SelectableLabelRenderer : ViewRenderer<SelectableLabel, UITextView>
+    {
+        UITextView uiTextView;
+
+        protected override void OnElementChanged(ElementChangedEventArgs<SelectableLabel> e)
+        {
+            base.OnElementChanged(e);
+
+            var label = (SelectableLabel)Element;
+            if (label == null)
+                return;
+
+            if (Control == null)
+            {
+                uiTextView = new UITextView();
+            }
+
+            uiTextView.Selectable = true;
+            uiTextView.Editable = false;
+            uiTextView.ScrollEnabled = false;
+            uiTextView.TextContainerInset = UIEdgeInsets.Zero;
+            uiTextView.TextContainer.LineFragmentPadding = 0;
+            uiTextView.BackgroundColor = UIColor.Clear;
+
+            // Initial properties Set
+            uiTextView.Text = label.Text;
+            uiTextView.TextColor = label.TextColor.ToUIColor();
+            switch (label.FontAttributes)
+            {
+                case FontAttributes.None:
+                    uiTextView.Font = UIFont.SystemFontOfSize(new nfloat(label.FontSize));
+                    break;
+                case FontAttributes.Bold:
+                    uiTextView.Font = UIFont.BoldSystemFontOfSize(new nfloat(label.FontSize));
+                    break;
+                case FontAttributes.Italic:
+                    uiTextView.Font = UIFont.ItalicSystemFontOfSize(new nfloat(label.FontSize));
+                    break;
+                default:
+                    uiTextView.Font = UIFont.BoldSystemFontOfSize(new nfloat(label.FontSize));
+                    break;
+            }
+
+            SetNativeControl(uiTextView);
+        }
+
+        protected override void OnElementPropertyChanged(object sender, PropertyChangedEventArgs e)
+        {
+            base.OnElementPropertyChanged(sender, e);
+
+            if (e.PropertyName == SelectableLabel.TextProperty.PropertyName)
+            {
+                if (Control != null && Element != null && !string.IsNullOrWhiteSpace(Element.Text))
+                {
+                    uiTextView.Text = Element.Text;
+                }
+            }
+            else if (e.PropertyName == SelectableLabel.TextColorProperty.PropertyName)
+            {
+                if (Control != null && Element != null)
+                {
+                    uiTextView.TextColor = Element.TextColor.ToUIColor();
+                }
+            }
+            else if (e.PropertyName == SelectableLabel.FontAttributesProperty.PropertyName
+                        || e.PropertyName == SelectableLabel.FontSizeProperty.PropertyName)
+            {
+                if (Control != null && Element != null)
+                {
+                    switch (Element.FontAttributes)
+                    {
+                        case FontAttributes.None:
+                            uiTextView.Font = UIFont.SystemFontOfSize(new nfloat(Element.FontSize));
+                            break;
+                        case FontAttributes.Bold:
+                            uiTextView.Font = UIFont.BoldSystemFontOfSize(new nfloat(Element.FontSize));
+                            break;
+                        case FontAttributes.Italic:
+                            uiTextView.Font = UIFont.ItalicSystemFontOfSize(new nfloat(Element.FontSize));
+                            break;
+                        default:
+                            uiTextView.Font = UIFont.BoldSystemFontOfSize(new nfloat(Element.FontSize));
+                            break;
+                    }
+                }
+            }
+        }
+    }
+}
